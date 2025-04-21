b720f017e3deec5ba43bf7f2a341a398ef8e0208
Chris Laplante <chris.laplante@agilent.com>
2025-04-20 21:20:21 -0400

    WIP

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,24 +1,7 @@
 use bincode::{Decode, Encode};
-use serde::Serialize;
-use serde::Deserialize;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[derive(Decode, Encode, Debug)]
 pub struct Test {
     pub wat: u32,
     pub version: f32,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
